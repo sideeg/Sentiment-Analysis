a4c9a9fd38b138175e6653050838f1ed47af6492
sideeg <satelite902@gmail.com>
2024-11-15 18:04:57 +0300

    Setting max rows and columns

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score
 from sklearn.metrics import precision_score, recall_score, f1_score
-from sklearn.metrics import plot_roc_curve
-
+# from sklearn.metrics import plot_roc_curve
+from sklearn.metrics import RocCurveDisplay
 # Importing LogisticRegression from sklearn
 from sklearn.linear_model import LogisticRegression
 
@@ -53,3 +53,10 @@ from xgboost import XGBClassifier
 from sklearn.metrics.pairwise import pairwise_distances
 
 from pprint import pprint
+
+
+# Setting max rows and columns
+pd.set_option('max_columns', 50)
+pd.set_option('max_rows', 50)
+pd.set_option('display.max_colwidth', 300)
+pd.set_option("display.precision", 2)
