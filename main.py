a35c65b97d260c291ff26a6aa71ecf4ae740da46
sideeg <satelite902@gmail.com>
2024-11-15 17:57:21 +0300

    add the needed packeges

---
+++ main.py
@@ -0,0 +1,55 @@
+import json
+import numpy as np
+import pandas as pd
+import re, nltk, spacy, string
+import en_core_web_sm
+# nlp = en_core_web_sm.load()
+nlp = spacy.load('en_core_web_sm',  disable=["parser", "ner"])
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from plotly.offline import plot
+import plotly.graph_objects as go
+import plotly.express as px
+
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+
+from nltk.tokenize import word_tokenize
+from collections import Counter
+import string
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+# import spellchecker
+# from spellchecker import SpellChecker
+
+from textblob import TextBlob
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+from imblearn.over_sampling import SMOTE
+
+# Import pickle to save and load the model
+import pickle
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
+from sklearn.metrics import confusion_matrix, classification_report, roc_auc_score
+from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import plot_roc_curve
+
+# Importing LogisticRegression from sklearn
+from sklearn.linear_model import LogisticRegression
+
+# Importing Random Forest Classifier from sklearn
+from sklearn.ensemble import RandomForestClassifier
+
+# importing libraries for XGBoost classifier
+import xgboost as xgb
+from xgboost import XGBClassifier
+
+from sklearn.metrics.pairwise import pairwise_distances
+
+from pprint import pprint
