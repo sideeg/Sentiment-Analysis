bb3e10de61a690014c4a3ead1b53294ac3587453
sideeg <satelite902@gmail.com>
2024-11-18 01:12:46 +0300

    Showing only the top 5 Product Names for the user

--- main.py
+++ main.py
@@ -978,3 +978,44 @@ user_final_rating.shape
 
 print(user_final_rating.head(2))
 
+# Fine-Tuning the Recommendation System and Recommendation of Top 5 Products
+# Create function to recommend top 5 products to any user
+def product_recommendations_user(user_name):
+  if user_name not in user_final_rating.index:
+    print(f"The User {user_name} does not exist. Please provide a valid user name")
+  else:
+    # Get top 20 recommended products from the best recommendation model
+    top20_recommended_products = list(user_final_rating.loc[user_name].sort_values(ascending=False)[0:20].index)
+    # Get only the recommended products from the prepared dataframe "df_sent"
+    df_top20_products = df_sent[df_sent.id.isin(top20_recommended_products)]
+    # For these 20 products, get their user reviews and pass them through TF-IDF vectorizer to convert the data into suitable format for modeling
+    X = tfidf.transform(df_top20_products["reviews_lemmatized"].values.astype(str))
+    # Use the best sentiment model to predict the sentiment for these user reviews
+    df_top20_products['predicted_sentiment'] = logreg.predict(X)
+    # Create a new column to map Positive sentiment to 1 and Negative sentiment to 0. This will allow us to easily summarize the data
+    df_top20_products['positive_sentiment'] = df_top20_products['predicted_sentiment'].apply(lambda x: 1 if x=="Positive" else 0)
+    # Create a new dataframe "pred_df" to store the count of positive user sentiments
+    pred_df = df_top20_products.groupby(by='name').sum()
+    pred_df.columns=['pos_sent_count']
+    # Create a column to measure the total sentiment count
+    pred_df['total_sent_count'] = df_top20_products.groupby(by='name')['predicted_sentiment'].count()
+    # Create a column that measures the % of positive user sentiment for each product review
+    pred_df['post_sent_percentage'] = np.round(pred_df['pos_sent_count']/pred_df['total_sent_count']*100,2)
+    # Return top 5 recommended products to the user
+    result = pred_df.sort_values(by='post_sent_percentage', ascending=False)[:5]
+    return result
+
+# Top 5 Products Recommendation
+# Take a sample username as input
+user_input = 'charlie'
+# 'Venkat'
+# 'debb'
+# 'evrydayhustla420'
+print(f"Printing the top 5 recommended products for the user: {user_input} along with each product's positive sentiment count, overall review count and positive sentiment %")
+print("\n")
+top5_reco_sent_reco_user = product_recommendations_user(user_input)
+top5_reco_sent_reco_user
+
+# Showing only the top 5 Product Names for the user
+print(f"Printing the top 5 recommended products list for the user: {user_input}")
+print(list(top5_reco_sent_reco_user.index))
